3adef60b7de52e7766bd2e9e1ac781e096e44c17
bamcasa <combeesang@gmail.com>
2024-11-23 18:25:02 +0900

    init

---
+++ src/main/java/com/example/youthseed/YouthseedApplication.java
@@ -0,0 +1,13 @@
+package com.example.youthseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthseedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthseedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/youthseed/YouthseedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youthseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthseedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
